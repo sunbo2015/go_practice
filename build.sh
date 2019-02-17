c40d4f08d1ae3596f4f60d7549ef6c2841358024
屋打头 <sunbo_1@foxmail.com>
2019-02-17 17:19:56 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,15 +2,7 @@
 
 set -eu
 
-VERSION=$(git describe --abbrev=0 --tags)
-REVCNT=$(git rev-list --count HEAD)
-DEVCNT=$(git rev-list --count $VERSION)
-if test $REVCNT != $DEVCNT
-then
-	VERSION="$VERSION.dev$(expr $REVCNT - $DEVCNT)"
-fi
-echo "VER: $VERSION"
-
+VERSION=$(git rev-list --count HEAD)
 GITCOMMIT=$(git rev-parse HEAD)
 BUILDTIME=$(date -u +%Y/%m/%d-%H:%M:%S)
 
